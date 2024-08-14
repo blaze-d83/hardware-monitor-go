2ff9c3f2519f6ca57f40b295402f080533f3765e
DarshanAware <144710362+blaze-d83@users.noreply.github.com>
2024-08-14 21:10:03 +0530

    Added styling

--- internal/templates/monitor_templ.go
+++ internal/templates/monitor_templ.go
@@ -31,27 +31,27 @@ func MonitorSection(timeStamp string, hostName string, totalMemory uint64, usedM
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<div hx-swap-oob=\"innerHTML:#update-timestamp\">")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<style type=\"text/css\">\r\n\t\t/* Container for Monitor Data */\r\n\t\t#monitor-data {\r\n\t\t\tdisplay: flex;\r\n\t\t\tflex-direction: column;\r\n\t\t\talign-items: center;\r\n\t\t\tmargin: 20px auto;\r\n\t\t\tpadding: 30px;\r\n\t\t\twidth: 80%;\r\n\t\t\tmax-width: 1200px;\r\n\t\t\tbackground-color: #ffffff;\r\n\t\t\tborder-radius: 12px;\r\n\t\t\tbox-shadow: 0 10px 20px rgba(0, 0, 0, 0.1);\r\n\t\t}\r\n\r\n\t\t/* Update Timestamp */\r\n\t\t#update-timestamp {\r\n\t\t\tfont-size: 16px;\r\n\t\t\tcolor: #888;\r\n\t\t\tmargin-bottom: 20px;\r\n\t\t\tfont-style: italic;\r\n\t\t}\r\n\r\n\t\t/* Data Sections */\r\n\t\t#system-data, #disk-data, #cpu-data {\r\n\t\t\twidth: 100%;\r\n\t\t\tmargin-bottom: 20px;\r\n\t\t\tpadding: 20px;\r\n\t\t\tborder-radius: 8px;\r\n\t\t\tbackground-color: #f8f9fa;\r\n\t\t\tborder: 1px solid #dee2e6;\r\n\t\t}\r\n\r\n\t\t#system-data h2, #disk-data h2, #cpu-data h2 {\r\n\t\t\tmargin-top: 0;\r\n\t\t\tfont-size: 1.5em;\r\n\t\t\tcolor: #343a40;\r\n\t\t}\r\n\r\n\t\t#system-data div, #disk-data div, #cpu-data div {\r\n\t\t\tmargin: 10px 0;\r\n\t\t\tfont-size: 1.1em;\r\n\t\t\tcolor: #495057;\r\n\t\t}\r\n\r\n\t\t/* Responsive Design */\r\n\t\t@media (max-width: 768px) {\r\n\t\t\t#monitor-data {\r\n\t\t\t\twidth: 95%;\r\n\t\t\t\tpadding: 20px;\r\n\t\t\t}\r\n\r\n\t\t\t#system-data, #disk-data, #cpu-data {\r\n\t\t\t\tpadding: 15px;\r\n\t\t\t}\r\n\t\t}\r\n\t</style><div hx-swap-oob=\"innerHTML:#update-timestamp\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
 		var templ_7745c5c3_Var2 string
 		templ_7745c5c3_Var2, templ_7745c5c3_Err = templ.JoinStringErrs(timeStamp)
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/templates/monitor.templ`, Line: 9, Col: 59}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/templates/monitor.templ`, Line: 67, Col: 60}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var2))
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</div><div hx-swap-oob=\"innerHTML:#system-data\"><div>Host name: ")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</div><div hx-swap-oob=\"innerHTML:#system-data\"><h2>System Information</h2><div>Host name: ")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
 		var templ_7745c5c3_Var3 string
 		templ_7745c5c3_Var3, templ_7745c5c3_Err = templ.JoinStringErrs(hostName)
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/templates/monitor.templ`, Line: 12, Col: 29}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/templates/monitor.templ`, Line: 71, Col: 24}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var3))
 		if templ_7745c5c3_Err != nil {
@@ -64,7 +64,7 @@ func MonitorSection(timeStamp string, hostName string, totalMemory uint64, usedM
 		var templ_7745c5c3_Var4 string
 		templ_7745c5c3_Var4, templ_7745c5c3_Err = templ.JoinStringErrs(fmt.Sprintf("%d", totalMemory))
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/templates/monitor.templ`, Line: 15, Col: 54}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/templates/monitor.templ`, Line: 74, Col: 49}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var4))
 		if templ_7745c5c3_Err != nil {
@@ -77,7 +77,7 @@ func MonitorSection(timeStamp string, hostName string, totalMemory uint64, usedM
 		var templ_7745c5c3_Var5 string
 		templ_7745c5c3_Var5, templ_7745c5c3_Err = templ.JoinStringErrs(fmt.Sprintf("%d", usedMemory))
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/templates/monitor.templ`, Line: 18, Col: 51}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/templates/monitor.templ`, Line: 77, Col: 47}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var5))
 		if templ_7745c5c3_Err != nil {
@@ -90,20 +90,20 @@ func MonitorSection(timeStamp string, hostName string, totalMemory uint64, usedM
 		var templ_7745c5c3_Var6 string
 		templ_7745c5c3_Var6, templ_7745c5c3_Err = templ.JoinStringErrs(os)
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/templates/monitor.templ`, Line: 21, Col: 16}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/templates/monitor.templ`, Line: 80, Col: 11}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var6))
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</div></div><div hx-swap-oob=\"innerHTML:#disk-data\"><div>Total Disk Space: ")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</div></div><div hx-swap-oob=\"innerHTML:#disk-data\"><h2>Disk Information</h2><div>Total Disk Space: ")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
 		var templ_7745c5c3_Var7 string
 		templ_7745c5c3_Var7, templ_7745c5c3_Err = templ.JoinStringErrs(fmt.Sprintf("%d", totalDiskSpace))
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/templates/monitor.templ`, Line: 26, Col: 61}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/templates/monitor.templ`, Line: 86, Col: 56}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var7))
 		if templ_7745c5c3_Err != nil {
@@ -116,20 +116,20 @@ func MonitorSection(timeStamp string, hostName string, totalMemory uint64, usedM
 		var templ_7745c5c3_Var8 string
 		templ_7745c5c3_Var8, templ_7745c5c3_Err = templ.JoinStringErrs(fmt.Sprintf("%d", freeDiskSpace))
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/templates/monitor.templ`, Line: 29, Col: 59}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/templates/monitor.templ`, Line: 89, Col: 54}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var8))
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</div></div><div hx-swap-oob=\"innerHTML:#cpu-data\"><div>CPU: ")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</div></div><div hx-swap-oob=\"innerHTML:#cpu-data\"><h2>CPU Information</h2><div>CPU: ")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
 		var templ_7745c5c3_Var9 string
 		templ_7745c5c3_Var9, templ_7745c5c3_Err = templ.JoinStringErrs(cpuModelName)
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/templates/monitor.templ`, Line: 34, Col: 27}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/templates/monitor.templ`, Line: 95, Col: 22}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var9))
 		if templ_7745c5c3_Err != nil {
@@ -142,7 +142,7 @@ func MonitorSection(timeStamp string, hostName string, totalMemory uint64, usedM
 		var templ_7745c5c3_Var10 string
 		templ_7745c5c3_Var10, templ_7745c5c3_Err = templ.JoinStringErrs(strconv.FormatUint(uint64(cores), 10))
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/templates/monitor.templ`, Line: 37, Col: 54}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/templates/monitor.templ`, Line: 98, Col: 49}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var10))
 		if templ_7745c5c3_Err != nil {
